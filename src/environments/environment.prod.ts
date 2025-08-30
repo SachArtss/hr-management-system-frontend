21e5820a8f3a1fb4609e0a71f60fe1a5e91de91b
Hadush Negasi <hadush7512@gmail.com>
2025-08-30 09:02:50 +0300

    updated the backend url to fetch data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hr-backend-1m4q.onrender.com/' // Your production backend URL
+  apiUrl: 'https://hr-backend-1m4q.onrender.com' // Your production backend URL
 };
